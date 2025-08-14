d2ecfaafc6035fe4db0ce0e9cebbea4a9fa8b19b
marcos <marcos.v.miranda8@aluno.senai.com>
2025-08-14 10:40:28 -0300

    Atividade senai PHP

---
+++ resto_divisao.php
@@ -0,0 +1,12 @@
+<?php
+    $numero1 = 10;
+    $numero2 = 3;
+   
+    $divisao = $numero1 / $numero2;
+    $resto = $numero1 % $numero2;
+
+    echo "A divisão de ". $numero1. " e " .$numero2. " é ".$divisao. ", e seu resto é ". $resto;
+    //echo $resto; // Saída: 1 (pois 10 dividido por 3 tem resto 1)
+    
+    
+    ?>
